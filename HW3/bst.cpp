8c27b80552465de6da1dfc27e3db24cfaafcd3d9
bmax <bmax1337@gmail.com>
2015-02-23 00:59:30 -0500

    Added test text file to use for numbers to insert into binary search tree. Currently grabs the numbers from the text file and puts them into a vector

--- HW3/bst.cpp
+++ HW3/bst.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <fstream>
 
-int main()
+using namespace std;
+int main ( int argc, char *argv[] )
 {
-cout << "yop";
+  vector<int> input;
+  if ( argc != 2 ) // argc should be 2 for correct execution
+    cout<<"usage: "<< argv[0] <<" <filename>\n";
+  else {
+    // We assume argv[1] is a filename to open
+    ifstream the_file ( argv[1] );
+    if ( !the_file.is_open() )
+      cout<<"Could not open file\n";
+    else {
+      int x;
+      // the_file.get ( x ) returns false if the end of the file
+      //  is reached or an error occurs
+      while ( the_file >> x )
+        input.push_back(x);
+    }
+    for (auto i : input) 
+    {
+      cout << i << endl;
+    }
+  }
+  // the_file is closed implicitly here
 }
 
